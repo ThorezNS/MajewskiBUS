e623c49eb5239ee87374e3e40a74413c156683f9
Maciej_Niemiec <niemiec.maciej@gmail.com>
2022-06-19 14:14:14 +0200

    Move popup text content from HTML file to JSON

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
-const popupWindow = () => {
-  const ticketsBtn = document.querySelector('.tickets-btn');
-  const popupBtn = document.querySelector('.popup__button');
-  const popup = document.querySelector('.popup');
+const domElements = {
+  ticketsBtn: document.querySelector('.tickets-btn'),
+  popupBtn: document.querySelector('.popup__button'),
+  popup: document.querySelector('.popup'),
+};
 
-  ticketsBtn.addEventListener('click', () => {
-    popup.classList.add('popup-active');
+const fetchJson = (path, callback) => {
+  fetch(path)
+    .then((response) => response.json())
+    .then((data) => callback(data));
+};
+
+const popupWindow = () => {
+  domElements.ticketsBtn.addEventListener('click', () => {
+    domElements.popup.classList.add('popup-active');
   });
 
-  popupBtn.addEventListener('click', () => {
-    popup.classList.remove('popup-active');
+  domElements.popupBtn.addEventListener('click', () => {
+    domElements.popup.classList.remove('popup-active');
+  });
+};
+
+const theTextOfThePopup = (data) => {
+  data.forEach((section) => {
+    const title = document.createElement('h2');
+    title.className = 'popup__title';
+    title.innerText = section.title;
+    domElements.popup.appendChild(title);
+
+    section.body.forEach((body) => {
+      const paragraph = document.createElement('p');
+      paragraph.innerText = body.paragraph;
+      domElements.popup.appendChild(paragraph);
+
+      body.bullet.forEach((bullet) => {
+        const bulletElement = document.createElement('span');
+        bulletElement.className = 'popup__bullet';
+        bulletElement.innerText = bullet;
+        paragraph.appendChild(bulletElement);
+      });
+    });
   });
 };
 
 popupWindow();
+fetchJson('./data/theTextOfThePopup.json', theTextOfThePopup);
